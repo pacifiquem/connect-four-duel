e4c00b65995895eedfae3cf95d7a36917afd7828
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-02-19 15:45:36 +0200

    feat: added move validator in PlayerVSPlayer

--- cli/classes/PlayerVsPlayer.js
+++ cli/classes/PlayerVsPlayer.js
@@ -18,26 +18,32 @@ class PlayerVsPlayer extends Game {
       // Get the player's move
       let col = await this.getPlayerMove(currentPlayer);
 
-      // Place the player's piece on the board
-      let row = this.ROWS - 1;
-      while (row >= 0 && this.board[row][col] !== ".") {
-        row--;
-      }
-
-      if (row < 0) {
-        console.log("Column is full, try again");
-      } else {
-        this.board[row][col] = currentPlayer;
-        if (this.checkWin(currentPlayer)) {
-          this.printBoard();
-          console.log(`${currentPlayer} wins!`);
-          return;
-        } else if (this.board.every((row) => row.every((col) => col !== "."))) {
-          this.printBoard();
-          console.log("Game over, tie!");
-          return;
+      if (this.isValidMove(col)) {
+        // Place the player's piece on the board
+        let row = this.ROWS - 1;
+        while (row >= 0 && this.board[row][col] !== ".") {
+          row--;
         }
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
+
+        if (row < 0) {
+          console.log("Column is full, try again");
+        } else {
+          this.board[row][col] = currentPlayer;
+          if (this.checkWin(currentPlayer)) {
+            this.printBoard();
+            console.log(`${currentPlayer} wins!`);
+            return;
+          } else if (
+            this.board.every((row) => row.every((col) => col !== "."))
+          ) {
+            this.printBoard();
+            console.log("Game over, tie!");
+            return;
+          }
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+        }
+      } else {
+        console.log("Invalid move, please try again.");
       }
     }
   }
